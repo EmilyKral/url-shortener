b64686ee02cd73bfdfe8f20ecc7b1902fbcb9a93
EmilyKral <emilykral@gmail.com>
2022-02-02 15:51:13 +0000

    added styling and cleaned up code

--- shortener/main/forms.py
+++ shortener/main/forms.py
@@ -3,8 +3,9 @@ from .models import URL
 
 
 class URLForm(forms.ModelForm):
-    original_url = forms.CharField()
-    
+    original_url = forms.CharField(widget=forms.TextInput(
+        attrs={"placeholder": "http://my-url.co.uk"}))
+
     class Meta:
         model = URL
         fields = ['original_url']
